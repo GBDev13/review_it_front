830b2e5abc9af5db05e113f770c9fc50496c4341
Gabriel Borges <gbdesigns13@gmail.com>
2021-06-14 20:16:34 -0300

    Add filters per category section

--- src/styles/HomeStyles.ts
+++ src/styles/HomeStyles.ts
@@ -6,3 +6,11 @@ export const HomeContainer = styled.section`
   display: flex;
   flex-direction: column;
 `;
+
+export const HomeContent = styled.main`
+  width: 100%;
+  height: 100%;
+  max-width: 95vw;
+  margin: 0 auto;
+  padding: 0 2rem;
+`;
