f885a81cc0f49d0049a3641a3e4892f14d12b455
Gabriel Borges <gbdesigns13@gmail.com>
2021-06-14 12:59:53 -0300

    updated theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,6 +2,9 @@ const theme = {
   colors: {
     background: '#140E33',
     text: '#fff',
+    textSecondary: '#968DCE',
+    card: '#241C4B',
+    border: '#413292',
     primary: '#6D55FD'
   }
 };
