fb7ba334c749765aa56a2a658571d35806c73355
viniciusoliveiras <vinitag190@gmail.com>
2021-06-15 12:51:15 -0300

    titulo das paginas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,28 @@
+import React from 'react';
+import Head from 'next/head';
+
 import Header from '../components/Header';
-import { HomeContainer, HomeContent } from '../styles/HomeStyles';
 import FilterList from '../components/FilterList';
 import CardGrid from '../components/Card/CardGrid';
 import MobileMenu from '../components/MobileMenu';
 
+import { HomeContainer, HomeContent } from '../styles/HomeStyles';
+
 export default function Home() {
   return (
-    <HomeContainer>
-      <MobileMenu />
-      <Header />
-      <HomeContent>
-        <FilterList />
-        <CardGrid />
-      </HomeContent>
-    </HomeContainer>
+    <>
+      <Head>
+        <title>review.it | home</title>
+      </Head>
+
+      <HomeContainer>
+        <MobileMenu />
+        <Header />
+        <HomeContent>
+          <FilterList />
+          <CardGrid />
+        </HomeContent>
+      </HomeContainer>
+    </>
   );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { toast } from 'react-toastify';
 
@@ -16,34 +17,40 @@ export default function Login() {
   };
 
   return (
-    <Container>
-      <LogoContent>
-        <h1>
-          review<span>.it</span>
-        </h1>
-      </LogoContent>
+    <>
+      <Head>
+        <title>review.it | login</title>
+      </Head>
 
-      <Content>
-        <form onSubmit={handleSubmit(handleSignIn)}>
-          <input
-            name="email"
-            type="email"
-            placeholder="E-mail"
-            required
-            {...register('email')}
-          />
+      <Container>
+        <LogoContent>
+          <h1>
+            review<span>.it</span>
+          </h1>
+        </LogoContent>
 
-          <input
-            name="password"
-            type="password"
-            placeholder="Senha"
-            required
-            {...register('password')}
-          />
+        <Content>
+          <form onSubmit={handleSubmit(handleSignIn)}>
+            <input
+              name="email"
+              type="email"
+              placeholder="E-mail"
+              required
+              {...register('email')}
+            />
 
-          <button type="submit">Entrar</button>
-        </form>
-      </Content>
-    </Container>
+            <input
+              name="password"
+              type="password"
+              placeholder="Senha"
+              required
+              {...register('password')}
+            />
+
+            <button type="submit">Entrar</button>
+          </form>
+        </Content>
+      </Container>
+    </>
   );
 }
