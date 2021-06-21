977fd730e391451c92646483d2b1ed0efd7f3f88
Gabriel Borges <gbdesigns13@gmail.com>
2021-06-20 21:44:19 -0300

    SEO adicionado

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -48,7 +48,45 @@ export default class MyDocument extends Document {
           />
 
           <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta httpEquiv="x-ua-compatible" content="IE=edge,chrome=1" />
+
+          <meta name="MobileOptimized" content="320" />
+          <meta name="HandheldFriendly" content="True" />
+          <meta name="theme-color" content="#6D55FD" />
+          <meta name="msapplication-TileColor" content="#6D55FD" />
+          <meta name="google" content="notranslate" />
+
+          <meta
+            property="og:title"
+            content="review.it - Revise seu código facilmente!"
+          />
+          <meta
+            property="og:description"
+            content="Plataforma feita para revisar seus códigos!"
+          />
+          <meta property="og:locale" content="pt_BR" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:site_name"
+            content="review.it - Revise seu código facilmente!"
+          />
+          <meta property="og:image" content="/ogimage.png" />
+          <meta property="og:image:secure_url" content="/ogimage.png" />
+          <meta property="og:image:alt" content="Thumbnail" />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+
+          <meta
+            name="twitter:title"
+            content="review.it - Revise seu código facilmente!"
+          />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:image" content="/ogimage.png" />
+          <meta name="twitter:image:src" content="/ogimage.png" />
+          <meta name="twitter:image:alt" content="Thumbnail" />
+          <meta name="twitter:image:width" content="1200" />
+          <meta name="twitter:image:height" content="620" />
         </Head>
         <body>
           <Main />
